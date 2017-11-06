80b692f4e6c806df3c02606bb39b16305255dc51
Kangyun Wang <wky@192.168.2.44>
2017-11-06 15:23:40 +0100

    Removed queue.js. use d3.queue

--- scripts/index.js
+++ scripts/index.js
@@ -2,17 +2,24 @@ d3.text('../data/to_germany_2014.csv', loadRefugee);
 // d3.text('../data/all_refugees12.csv', loadRefugee);
 // d3.text('../data/all_refugees12-17.csv', loadRefugee);
 
-async function loadRefugee(err, res) {
+async function loadRefugee(err, ref, test) {
 	if (err) return err;
+	console.log(test);
 
 	// Set cleaned global data available
-	refugeeData = await cleanRefugee(res);
+	refugeeData = await cleanRefugee(ref);
 	timeData = await cleanTime(refugeeData)
 
 	// Render the map
-	d3.json('data/ne_50m_admin_0_countries_lakes.json', loadMap);
+	// d3.json('data/ne_50m_admin_0_countries_lakes.json', loadMap);
 
 	// render teh timeline
-	await renderTimeLine();
+	// await renderTimeLine();
 
-} 
+}
+
+d3.queue()
+	// queue()
+	.defer(d3.text, '../data/to_germany_2014.csv')
+	.defer(d3.json, '../data/test.json')
+	.await(loadRefugee)
