5e726246f31a09775fd2be8bcd5786005861e949
Kangyun Wang <wky@192.168.2.44>
2017-11-06 20:36:25 +0100

    gitignore update. Minor change to test data. Fix global widht/height variable

--- utils/constants.js
+++ utils/constants.js
@@ -1,11 +1,16 @@
-var winWidth = window.innerWidth;
-var winHeight = window.innerHeight - 120;
+// var winWidth = window.innerWidth;
+var winWidth = document.body.clientWidth;
+// var winHeight = window.innerHeight - 120;
+var winHeight = document.body.clientHeight - 120;
 
 // Data vars
 var refugeeData;
 var timeData;
+
 var directionMapping = [];
 var countryCenter;
+
+var journeyData;
 
 // Animation var
 var transDur = 1000;
